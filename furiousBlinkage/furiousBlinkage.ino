146ed3c41f0ba88b5a398de6e6941103c12ed2ad
John Evans <jhevans28@gmail.com>
2015-01-30 22:04:04 +0000

    did a bit more

--- furiousBlinkage/furiousBlinkage.ino
+++ furiousBlinkage/furiousBlinkage.ino
@@ -1,21 +1,48 @@
 // Blink
 // Uses LED L on the Arduino board
+#define arrayLength 6
 
 int ledPin = 13;  // LED is connected with digital pin 13
+int numbers[] = {1,4,6,7,8,24};
 
 // the setup routine configures the digital port
 // This routine is only executed once at program start!
 
 void setup(){
+  Serial.begin(9600);
   // The pin is configured as output
   pinMode(ledPin, OUTPUT);
+  Serial.println("What say you men?");
+  Serial.println();
 }
 
 // The main program is an infinite loop
 void loop(){
-  digitalWrite(ledPin, HIGH);
-  delay(50);
-  digitalWrite(ledPin, LOW);
-  delay(50);
+  int sum = 0;
+  for(int i=0; i<arrayLength; i++){
+    Serial.print(numbers[i]);
+    if(i != arrayLength -1){
+      Serial.print("+");
+    }
+    sum += numbers[i];
+  }
+  Serial.print(" yields ");
+  Serial.print(sum);
+  Serial.println(" sir!");
+  Serial.println();
+
+  int count = 0;
+  while(true){
+    
+    if(count == 100){
+      Serial.println("You should probably turn me off now");
+    }
+    digitalWrite(ledPin, HIGH);
+    delay(50);
+    digitalWrite(ledPin, LOW);
+    delay(50);
+    count++;
+    continue;
+  }
 }
 
