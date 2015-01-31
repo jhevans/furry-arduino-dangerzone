b43f891c07db60fce3cecdf0987b004e346c53b2
John Evans <jhevans28@gmail.com>
2015-01-31 01:05:06 +0000

    Created a Morse code encoder which flashes the on board led and outputs dots and dashes to the console

---
+++ morseEncoder/morseEncoder.ino
@@ -0,0 +1,148 @@
+// Morse encoder
+
+char encoding[][7] = {
+  // A
+  ".-    ", 
+  //B	
+  "-...  ",
+  //C	
+  "-.-.  ",
+  //D	
+  "-..   ",
+  //E	
+  ".     ",
+  //F	
+  "..-.  ",
+  //G	
+  "--.   ",
+  //H	
+  "....  ",
+  //I	
+  "..    ",
+  //J	
+  ".---  ",
+  //K	
+  "-.-   ",
+  //L	
+  ".-..  ",
+  //M	
+  "--    ",
+  //N	
+  "-.    ",
+  //O	
+  "---   ",
+  //P	
+  ".--.  ",
+  //Q	
+  "--.-  ",
+  //R	
+  ".-.   ",
+  //S	
+  "...   ",
+  //T	
+  "-     ",
+  //U	
+  "..-   ",
+  //V	
+  "...-  ",
+  //W	
+  ".--   ",
+  //X	
+  "-..-  ",
+  //Y	
+  "-.--  ",
+  //Z	
+  "--..  ",
+};
+
+int ledPin = 13;
+int timeUnit = 25;
+int dotLength = timeUnit;
+int dashLength = 3*timeUnit;
+int interElementGapLength = timeUnit;
+int letterGapLength = 2*timeUnit;  
+int spaceLength = 4*timeUnit;
+int ucOffset = 65;
+int lcOffset = 97;  // Offset for lower case characters from lower case offset
+
+char input;
+byte correctedInput;
+
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Send me strings and I shall Morsinate them!");
+}
+
+void loop(){
+  byte inputByte;
+//  char morseString[7];
+  if(Serial.available() > 0){
+    input = Serial.read();
+//    Serial.print("input char: ");
+//    Serial.println(input);
+    if(input == 32){  // space
+      space();
+      return;
+    } else if(input == 10){  // newline
+      Serial.println();
+      return;
+    }
+    inputByte = charToNormalisedByte(input);
+//    Serial.print("encoded output: ");
+//    Serial.print(encoding[inputByte]);
+    // TODO: refactor this into a function
+    for(int i=0; i<7; i++){
+      switch(encoding[inputByte][i]){
+        case '.':
+          dot();
+          delay(interElementGapLength);
+          break;
+        case '-':
+          dash();
+          delay(interElementGapLength);
+          break;
+        default:
+          continue;      
+      }
+    }
+    delay(letterGapLength);
+    Serial.print(' ');
+  }
+}
+
+byte charToNormalisedByte(char sourceChar){
+  byte inputByte;
+  inputByte = int(sourceChar);
+  if(inputByte >= lcOffset){
+    correctedInput = inputByte - lcOffset;
+  } else if( inputByte >= ucOffset){
+    correctedInput = inputByte - ucOffset;
+  }
+    
+//    Serial.print("corrected input byte: ");
+//    Serial.println(correctedInput);
+  return correctedInput;  
+}
+
+void dot(){
+  blinkOnce(dotLength);
+  Serial.print('.');
+}
+
+void dash(){
+  blinkOnce(dashLength);
+  Serial.print('-');
+}
+
+void space(){
+  delay(spaceLength);
+  Serial.print("   ");
+}
+
+void blinkOnce(int delayLength){
+    digitalWrite(ledPin, HIGH);
+    delay(delayLength);
+    digitalWrite(ledPin, LOW);
+}
