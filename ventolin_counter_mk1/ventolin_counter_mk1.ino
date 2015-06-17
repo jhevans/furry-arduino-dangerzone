159c13e90cf532defdb7157d1ac3faae7f7eab0c
John Evans <jhevans28@gmail.com>
2015-06-17 19:51:40 +0100

    basic logic applied, in no way optimised

--- ventolin_counter_mk1/ventolin_counter_mk1.ino
+++ ventolin_counter_mk1/ventolin_counter_mk1.ino
@@ -6,34 +6,64 @@ int i,x;
 int RED_LED = 11;  
 int AMBER_LED = 12;
 int GREEN_LED = 13;
-int BTN1 = 10;
+int BTN = 10;
+int DELAY = 20;
 
 int GREEN_LIMIT = 150;
 int AMBER_LIMIT = 175;
+//int GREEN_LIMIT = 5;
+//int AMBER_LIMIT = 7;
+
+int count = 0;
+int btnValue1, btnValue2 = 0;
 
 void setup(){
-  Serial.begin(9600);
+//  Serial.begin(9600);
   pinMode(RED_LED, OUTPUT);
   pinMode(AMBER_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   
-  pinMode(BTN1, INPUT);
+  pinMode(BTN, INPUT);
+  digitalWrite(BTN, HIGH);
 }
 
 void loop(){
-  int count = 0;
-  int delayLength = 20;
   while(true){
-    count++;
-    if(count < GREEN_LIMIT) blinkOnce(GREEN_LED, delayLength);
-    else if (count < AMBER_LIMIT) blinkOnce(AMBER_LED, delayLength);
-    else blinkOnce(RED_LED, delayLength);
+//    printBtn();
+//    Serial.print("count: ");
+//    Serial.println(count);
+    detectButtonPress();
+    if(count < GREEN_LIMIT) blinkOnce(GREEN_LED, DELAY);
+    else if (count < AMBER_LIMIT) blinkOnce(AMBER_LED, DELAY);
+    else blinkOnce(RED_LED, DELAY);
   }
 }
 
-void blinkOnce(int pin, int delayLength){
+void printBtn(){
+  Serial.println(digitalRead(BTN));
+}
+
+void detectButtonPress(){
+  btnValue1 = digitalRead(BTN);
+//  Serial.print("btnValue1: ");
+//  Serial.println(btnValue1);
+  if(btnValue1 == LOW){
+    delay(50);
+    btnValue2 = digitalRead(BTN);
+//    Serial.print("btnValue1: ");
+//    Serial.println(btnValue2);
+    if(btnValue2 == LOW){
+      count++;
+      while(!digitalRead(BTN)){
+        // Do nothing
+      }
+    }
+  }
+}
+
+void blinkOnce(int pin, int DELAY){
     digitalWrite(pin, HIGH);
-    delay(delayLength);
+    delay(DELAY);
     digitalWrite(pin, LOW);
 }
 
